7bdcc61c0934e495e11b6bcf9867456298896b13
Tom Howard <tom@windyroad.com.au>
2015-04-02 20:51:02 +1100

    fixed package

---
+++ src/main/java/au/com/windyroad/servicegateway/ServiceGatewayApplication.java
@@ -0,0 +1,12 @@
+package au.com.windyroad.servicegateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServiceGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceGatewayApplication.class, args);
+    }
+}
