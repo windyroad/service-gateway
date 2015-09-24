3d19941e2fac2a020956033bdffeac415f042982
Tom Howard <tom@windyroad.com.au>
2015-09-25 07:11:49 +1000

    WIP UI

--- src/main/resources/static/js/hello.js
+++ src/main/resources/static/js/hello.js
@@ -1,28 +1,62 @@
-angular.module('todoApp', [])
-  .controller('TodoListController', function() {
-    var todoList = this;
-    todoList.todos = [
+var app = angular.module('serviceGateway', []);
+
+/*
+app.controller('EntityController', function() {
+    var controller = this;
+    controller.todos = [
       {text:'learn angular', done:true},
       {text:'build an angular app', done:false}];
  
-    todoList.addTodo = function() {
-      todoList.todos.push({text:todoList.todoText, done:false});
-      todoList.todoText = '';
+    controller.addTodo = function() {
+      controller.todos.push({text:controller.todoText, done:false});
+      controller.todoText = '';
     };
  
-    todoList.remaining = function() {
+    controller.remaining = function() {
       var count = 0;
-      angular.forEach(todoList.todos, function(todo) {
+      angular.forEach(controller.todos, function(todo) {
         count += todo.done ? 0 : 1;
       });
       return count;
     };
  
-    todoList.archive = function() {
-      var oldTodos = todoList.todos;
-      todoList.todos = [];
+    controller.archive = function() {
+      var oldTodos = controller.todos;
+      controller.todos = [];
       angular.forEach(oldTodos, function(todo) {
-        if (!todo.done) todoList.todos.push(todo);
+        if (!todo.done) controller.todos.push(todo);
       });
     };
   });
+  */
+app.controller('EntityController', function($scope, $http) {
+  var controller = this;
+  
+  $http.get('/admin/proxies').success(function(data) {
+    controller.entity = data;
+  })
+  controller.todos = [
+    {text:'learn angular', done:true},
+      {text:'build an angular app', done:false}];
+ 
+    controller.addTodo = function() {
+      controller.todos.push({text:controller.todoText, done:false});
+      controller.todoText = '';
+    };
+ 
+    controller.remaining = function() {
+      var count = 0;
+      angular.forEach(controller.todos, function(todo) {
+        count += todo.done ? 0 : 1;
+      });
+      return count;
+    };
+ 
+    controller.archive = function() {
+      var oldTodos = controller.todos;
+      controller.todos = [];
+      angular.forEach(oldTodos, function(todo) {
+        if (!todo.done) controller.todos.push(todo);
+      });
+    };
+});
